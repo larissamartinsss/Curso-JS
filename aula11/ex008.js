0e4096f47b69fa685f7a7ab01fc458a4ee583bd9
Larissa Martins <larissamartins.job@gmail.com>
2021-04-15 19:33:17 -0400

    trying.

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.2
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //Condição simples!
+    console.log('Você utrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando sinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'França'
+console.log(`Vivendo em ${país}`)
+if(país == 'BR') {
+    console.log('Você é Brasileira')
+} else {
+    console.log('Você é estrageiro')
+}
